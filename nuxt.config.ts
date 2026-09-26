b6e5cb16319cbb438749a9dbb67508a7d8b766d4
truonghoangthuan <tthun32@gmail.com>
2026-09-26 23:42:27 +0700

    chore: update vercel speed insight

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
     }
   },
 
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@primevue/nuxt-module', '@nuxtjs/supabase', '@vercel/speed-insights'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@primevue/nuxt-module', '@nuxtjs/supabase', '@vercel/speed-insights/nuxt'],
 
   supabase: {
     redirect: false,
